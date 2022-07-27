bcafbf7eb8018d24375d4cbc7b8aa3566220c553
Katz <Dalitsocae@gmail.com>
2022-07-27 09:06:14 +0200

    Try changing way function works, to check if pass in existing Userlis

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 #Ask user for their username
 Username = input("Hello, User. Please enter your username here: ")
-Pass = input("Please input your password here :")
+Pass = input("Please input your password here: ")
 
+Userlist = {
+    "Katz": 123,
+    "Juan": 123,
+    "Snek": 123
+}
 
 
 #Authenticate the user on if they are Katz
 def is_valid_credentials(u,pw):
-    if u == "Katz" and pw == "123":
+    '''if u == "Katz" and pw == "123":
         print("Your deepest, darkest secret")
     else:
+        print("Access Denied")'''
+    auth = {u,pw}
+
+    if str(u) in Userlist and int(pw) in Userlist:
+        print("Your deepest, darkest secret")
+    
+    else:
         print("Access Denied")
+    
 
 
 
